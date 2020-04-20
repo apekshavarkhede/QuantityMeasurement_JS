864c818bf2adba6761e0701ba145f4c6d84f7e78
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-04-20 22:38:43 +0530

    Added: 0 feet and null are not equal

--- test/QuantityTest.js
+++ test/QuantityTest.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 var quantity = require('../quantity')
 
 describe('Testing for Quantity Check', function () {
@@ -10,5 +10,9 @@ describe('Testing for Quantity Check', function () {
     it('should return false if quantities are not equal', function () {
         let quantityCheck = quantity.checkQuantity(1, 0);
         assert.notEqual(true, quantityCheck)
+    })
+
+    it('given 0 Feet and null should return null ', function () {
+        assert.isNull(null, quantity.checkQuantity(0, null))
     })
 })
