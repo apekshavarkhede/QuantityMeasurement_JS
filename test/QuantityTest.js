c78ae80a29a34782b91251f1d90dd87bbf49121d
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-04-20 22:58:54 +0530

    Added: check type of length

--- test/QuantityTest.js
+++ test/QuantityTest.js
@@ -3,16 +3,18 @@ var quantity = require('../quantity')
 
 describe('Testing for Quantity Check', function () {
     it('should return true if quantities are equal', function () {
-        let quantityCheck = quantity.checkQuantity(0, 0);
-        assert.equal(true, quantityCheck)
+        assert.equal(quantity.feetConverter(0), quantity.feetConverter(0))
     })
 
     it('should return false if quantities are not equal', function () {
-        let quantityCheck = quantity.checkQuantity(1, 0);
-        assert.notEqual(true, quantityCheck)
+        assert.notEqual(quantity.feetConverter(1), quantity.feetConverter(0))
     })
 
     it('given 0 Feet and null should return null ', function () {
-        assert.isNull(null, quantity.checkQuantity(0, null))
+        assert.isNull(null, quantity.feetConverter(0))
+    })
+
+    it('given 0 Feet should return type as number ', function () {
+        assert.typeOf(quantity.feetConverter(0), 'number')
     })
 })
