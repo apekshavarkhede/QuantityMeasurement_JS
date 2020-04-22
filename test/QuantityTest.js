cab7ea26ae1f1602fdde46e48a67d27047d2295d
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-04-22 16:26:48 +0530

    Added: add 1 liter and 1000 ml

--- test/QuantityTest.js
+++ test/QuantityTest.js
@@ -114,13 +114,17 @@ describe('Testing for Quantity Check', function () {
 
     it('given 1 gallon and 1 gallon when add should return 7.56 liters', function () {
         let resultOfAddition = volume.addVolumes(volume.gallonConverter(1), volume.gallonConverter(1))
-        assert.equal(7.56,resultOfAddition)
+        assert.equal(7.56, resultOfAddition)
     })
 
     it('given 1 gallon and 3.78 liters when add should return 7.56 liters', function () {
         let resultOfAddition = volume.addVolumes(volume.gallonConverter(1), volume.literConverter(3.78))
-        assert.equal(7.56,resultOfAddition)
+        assert.equal(7.56, resultOfAddition)
     })
 
+    it('given 1 liter and 1000 ml when add should return 2 liters', function () {
+        let resultOfAddition = volume.addVolumes(volume.literConverter(1), volume.mlConverter(1000))
+        assert.equal(2, resultOfAddition)
+    })
 
 })
